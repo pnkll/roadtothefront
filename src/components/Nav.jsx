3e928d0fff923f1aeff5bd49bbe2cef74f31f32a
Evgeny <kavasakya@gmail.com>
2021-11-08 23:30:55 +0400

    fromHDD

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+function Nav(){
+    return (
+        <nav className='nav'>
+        <div><a href=''>Profile</a></div>
+        <div><a href=''>Messages</a></div>
+        <div><a href=''>Settings</a></div>
+        <div><a href=''>Music</a></div>
+      </nav>
+    )
+}
+
+export default Nav
